8a31bbc85b48e5da354795f3efbafb35829a38cc
Reon Fernandes <159623954+FernandesReon@users.noreply.github.com>
2024-02-14 11:10:18 +0530

    Servlet Files.
    These files are responsible for the server-side execution of code.

---
+++ Login.java
@@ -0,0 +1,27 @@
+package com.example.authentication;
+
+import java.io.*;
+
+import com.example.DAO.UserFunction;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "login", value = "/login")
+public class Login extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        UserFunction user = new UserFunction();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (user.isValidUser(username, password)){
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("welcome.jsp");
+        }
+        else{
+            response.sendRedirect("login.jsp?error=1");
+        }
+    }
+}
